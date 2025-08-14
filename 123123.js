ea16097b3a84a174a23fe2dcbd1ce0e543939cc0
mang_ne <133965302+zoamk@users.noreply.github.com>
2025-08-14 17:36:27 +0900

    123123
    123123

---
+++ 123123.js
@@ -0,0 +1,7 @@
+let bunho03 = 3; // 값을 할당 변수의 자료형 숫자형
+console.log("bunho03========================")
+console.log(typeof bunho03);
+
+let bunho04 = "3"; // 값을 문자로 할당,변수의 자료형 문자형
+console.log("bunho04========================")
+console.log(typeof bunho04);
